12c4e698228286dc4ae1d5de81fe823a226dd152
Snorri Sturluson <snorri.sturluson@ccpgames.com>
2017-10-07 09:57:04 +0000

    Added tokenizer.

---
+++ src/tokens.erl
@@ -0,0 +1,113 @@
+%%%-------------------------------------------------------------------
+%%% @author snorri
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 30. Sep 2017 15:55
+%%%-------------------------------------------------------------------
+-module(tokens).
+-author("snorri").
+
+%% API
+-export([tokenize/1]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+token_type(Char) when Char >= $0 andalso Char =< $9 ->
+    number;
+
+token_type(Char) when Char == $  orelse Char == $\n ->
+    whitespace;
+
+token_type(${) ->
+    open_brace;
+
+token_type($}) ->
+    close_brace;
+
+token_type($[) ->
+    open_square_bracket;
+
+token_type($]) ->
+    close_square_bracket;
+
+token_type($,) ->
+    comma;
+
+token_type($") ->
+    qoute;
+
+token_type($:) ->
+    colon;
+
+token_type(_) ->
+    other.
+
+tokenize(<<>>) ->
+    {none, <<>>};
+
+tokenize(<<Char>>) ->
+    tokenize(Char, <<>>);
+
+tokenize(<<First, Rest/binary>>) ->
+    tokenize(First, Rest).
+
+consume(number, [First], Rest) ->
+    Input = <<First, Rest/binary>>,
+    {FloatValue, FloatRemainder} = string:to_float(Input),
+    case FloatValue of
+        error ->
+            {IntValue, IntRemainder} = string:to_integer(Input),
+            case IntValue of
+                error ->
+                    {{error, none}, Input};
+                _ ->
+                    {{number, IntValue}, IntRemainder}
+            end;
+        _ ->
+            {{number, FloatValue}, FloatRemainder}
+    end;
+
+consume(whitespace, [First], Rest) ->
+    T = token_type(First),
+    consume_whitespace(T, First, Rest);
+
+consume(qoute, [_Qoute], <<First, Rest/binary>>) ->
+    T = token_type(First),
+    consume_until_qoute(T, <<>>, First, Rest);
+
+consume(TokenType, [_First], Rest) ->
+    {TokenType, Rest}.
+
+consume_whitespace(whitespace, _Prev, <<>>) ->
+    {whitespace, <<>>};
+
+consume_whitespace(whitespace, _Prev, <<Last>>) ->
+    T = token_type(Last),
+    consume_whitespace(T, Last, <<>>);
+
+consume_whitespace(whitespace, _Prev, <<First, Rest/binary>>) ->
+    T = token_type(First),
+    consume_whitespace(T, First, Rest);
+
+consume_whitespace(Tokentype, Prev, Rest) ->
+    consume(Tokentype, [Prev], Rest).
+
+consume_until_qoute(qoute, Acc, _Qoute, Rest) ->
+    {{qouted_string, Acc}, Rest};
+
+consume_until_qoute(_TokenType, _Acc, _Prev, <<>>) ->
+    {error};
+
+consume_until_qoute(_TokenType, Acc, Prev, <<Last>>) ->
+    T = token_type(Last),
+    consume_until_qoute(T, <<Acc/binary, Prev>>, Last, <<>>);
+
+consume_until_qoute(_TokenType, Acc, Prev, <<First, Rest/binary>>) ->
+    T = token_type(First),
+    consume_until_qoute(T, <<Acc/binary, Prev>>, First, Rest).
+
+tokenize(First, Rest) ->
+    T = token_type(First),
+    consume(T, [First], Rest).
